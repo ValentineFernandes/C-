756a919b3168d3698f4665edd3f9d951245aee52
Valentine Fernandes <valentineinfantinejohn@gmail.com>
2022-04-24 01:52:47 -0800

    Update circle.cpp

--- circle.cpp
+++ circle.cpp
@@ -2,15 +2,14 @@
 using namespace std;
 int main()
 {
-int rad;
-
-float PI = 3.14, area, ci;
-cout<<"Enter radius of circle: ";
-cin>>rad;
-area = PI * rad * rad;
-cout<<"Area of circle "<< area<<endl;
-ci = 2 * PI * rad;
-cout<<"Circumference of circle "<< ci<<endl;
-return (0);
+  int rad;
+  float PI = 3.14, area, ci;
+  cout<<"Enter radius of circle: ";
+  cin>>rad;
+  area = PI * rad * rad;
+  cout<<"Area of circle "<< area<<endl;
+  ci = 2 * PI * rad;
+  cout<<"Circumference of circle "<< ci<<endl;
+  return (0);
 }
 
