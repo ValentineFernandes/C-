14199977cfc5c7289e3454ff41c0a3cd9d069e44
Valentine Fernandes <valentineinfantinejohn@gmail.com>
2022-04-24 01:57:45 -0800

    Update percent.cpp

--- percent.cpp
+++ percent.cpp
@@ -1,18 +1,13 @@
 #include<iostream>
 using namespace std;
-
 int main()
 {
    int s1, s2, s3, s4, s5, sum, total = 500;
    float per;
-
    cout<<"\nEnter marks of 5 subjects : ";
    cin>>s1>>s2>>s3>>s4>>s5;
-
    sum = s1 + s2 + s3 + s4 + s5;
-   
    per = (sum * 100) / total;
    cout<<"\nStudent Percentage Is : "<< per<<endl;
-
    return (0);
 }
