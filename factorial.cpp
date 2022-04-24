37dd4095fb6d27ca919e62a5e53dcd1152989722
Valentine Fernandes <valentineinfantinejohn@gmail.com>
2022-04-24 01:47:24 -0800

    Create factorial.cpp

---
+++ factorial.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+ unsigned long long int fact=1;
+  int i,num;
+  
+ cout<<"Enter The Number. You Want Factorial:";
+  cin>>num;
+  
+ for(i=1;i<=num;i++)
+    {
+        fact=fact*i;
+    }
+    
+ cout<<"\nFactorial of "<<num<<" Is = "<<fact<<endl;
+    return 0;
+
+}
