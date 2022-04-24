fbc5e0ee975dedb119341427900ff3493f3e4b7f
Valentine Fernandes <valentineinfantinejohn@gmail.com>
2022-04-24 01:58:15 -0800

    Update percentage.cpp

--- percentage.cpp
+++ percentage.cpp
@@ -3,12 +3,9 @@ using namespace std;
 int main()
 {
    int sub,marks,n,i,sum=0,tmp=0,arr[10],Percentage;
-
    cout<<"\nEnter number of subject : \n";
    cin>>n;
-   
    tmp=n*100;
-   
    cout<<"\nEnter The Marks: \n";
    for(i=0;i<n;i++)
    {
