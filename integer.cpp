61f6470da59fe3eed5579cddf34341f7d642116a
Valentine Fernandes <valentineinfantinejohn@gmail.com>
2022-04-24 01:48:57 -0800

    Create integer.cpp

---
+++ integer.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  int num;
+  cout<<"\nEnter The Number .\n";
+  cin>>num;
+  cout<<"\nOutpout is \n";
+  cout<<num<<"  ,"<<num*num<<"  ,"<<num*num*num<<endl;
+  return 0;
+
+}
