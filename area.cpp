98316453660cb448e6c3166630a2aee4f86a6d49
Valentine Fernandes <valentineinfantinejohn@gmail.com>
2022-04-24 01:51:40 -0800

    Update area.cpp

--- area.cpp
+++ area.cpp
@@ -3,10 +3,8 @@ using namespace std;
 int main()
 {
     float b,h,area;
-
     cout<<"Enter Height and Base Of Triangle : ";
     cin>>h>>b;
-
     area = (h*b)/2;
     cout<<"Area of Triangle is:"<<area<<endl;
     return 0;
