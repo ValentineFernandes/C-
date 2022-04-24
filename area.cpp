4103a71bff9bc5b03c944b43e3bb6acd30d9c9b5
Valentine Fernandes <valentineinfantinejohn@gmail.com>
2022-04-24 01:31:37 -0800

    Create area.cpp

---
+++ area.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float b,h,area;
+
+    cout<<"Enter Height and Base Of Triangle : ";
+    cin>>h>>b;
+
+    area = (h*b)/2;
+    cout<<"Area of Triangle is:"<<area<<endl;
+    return 0;
+
+}
