640d1819540f11f568c39b9f43549fe20491e92c
Valentine Fernandes <valentineinfantinejohn@gmail.com>
2022-04-24 02:00:23 -0800

    Update temperature.cpp

--- temperature.cpp
+++ temperature.cpp
@@ -3,13 +3,9 @@ using namespace std;
 int main()
 {
  float cel, fah;
-
  cout<<"\nEnter Temp in Celsius : ";
  cin>>cel;
-
  fah = (1.8 * cel) + 32;
  cout<<"\nTemperature in Fahrenheit : "<< fah;
-
  return (0);
-
 }
