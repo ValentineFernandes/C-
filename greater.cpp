2e7ef2dbdd7945a48544e3271b61bce08b2f10ce
Valentine Fernandes <valentineinfantinejohn@gmail.com>
2022-04-24 01:34:51 -0800

    Create greater.cpp

---
+++ greater.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   double num1, num2, num3;
+
+    cout<<"Enter Three Numbers: \n";
+    cin>>num1>>num2>>num3;
+
+   if (num1 > num2 && num1 > num3)
+      cout<<"\nGreatest Number Is :"<<num1;
+
+   else if (num2 > num3 && num2 > num1)
+      cout<<"\nGreatest Number Is :"<<num2;
+
+   else if (num1 > num1 && num3 > num2)
+      cout<<"\nGreatest Number Is :"<<num1;
+      
+   else if ((num1==num2) && (num2==num3) && (num3==num1))
+     cout<<"\nAll Are Equal"; 
+   
+
+}
