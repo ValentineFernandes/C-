c6ac5e50312651407496bcf257c1bb51a1b9ee7b
Valentine Fernandes <valentineinfantinejohn@gmail.com>
2022-04-24 01:40:36 -0800

    Create temperature.cpp

---
+++ temperature.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+ float cel, fah;
+
+ cout<<"\nEnter Temp in Celsius : ";
+ cin>>cel;
+
+ fah = (1.8 * cel) + 32;
+ cout<<"\nTemperature in Fahrenheit : "<< fah;
+
+ return (0);
+
+}
