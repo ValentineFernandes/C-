11fdef9aac619696fc273edae7cff8d8db4d013f
Valentine Fernandes <valentineinfantinejohn@gmail.com>
2022-04-24 01:42:48 -0800

    Create even-odd.cpp

---
+++ even-odd.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+ int a;
+ cout<<"Enter The Number You Want To Check : \n";
+ cin>>a;
+
+ if(a<0)
+ {
+   cout<<"Number Is Negative:\n";
+ }
+ else if(a>0)
+ {
+   cout<<"Number Is Possitive:\n";
+ } 
+ else
+ {
+  cout<<"You Enter Zero :\n";
+  }
+ return 0;
+
+}
