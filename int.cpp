6388b4f402375ba090a7b640479b249ad6ef716c
Valentine Fernandes <valentineinfantinejohn@gmail.com>
2022-04-24 01:56:23 -0800

    Update int.cpp

--- int.cpp
+++ int.cpp
@@ -2,13 +2,13 @@
 using namespace std;
 int main()
 {
-  int num,a,b,c;
-  cout<<"\nEnter The Number .\n";
-  cin>>num;
-  a=pow(num,1);
-  b=pow(num,2);
-  c=pow(num,3);
-  cout<<"\nOutpout is \n";
-  cout<<a<<"  ,"<<b<<"  ,"<<c<<endl;
-  return 0;
+   int num,a,b,c;
+   cout<<"\nEnter The Number .\n";
+   cin>>num;
+   a=pow(num,1);
+   b=pow(num,2);
+   c=pow(num,3);
+   cout<<"\nOutpout is \n";
+   cout<<a<<"  ,"<<b<<"  ,"<<c<<endl;
+   return 0;
 }
