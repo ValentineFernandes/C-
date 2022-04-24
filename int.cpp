bbc9c3c41b451e325d71953b218f95a292b3b919
Valentine Fernandes <valentineinfantinejohn@gmail.com>
2022-04-24 01:49:28 -0800

    Create int.cpp

---
+++ int.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  int num,a,b,c;
+  cout<<"\nEnter The Number .\n";
+  cin>>num;
+  a=pow(num,1);
+  b=pow(num,2);
+  c=pow(num,3);
+  cout<<"\nOutpout is \n";
+  cout<<a<<"  ,"<<b<<"  ,"<<c<<endl;
+  return 0;
+}
