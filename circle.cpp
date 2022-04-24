e0a4cc7bed56fce9fda55d122155307f636a713d
Valentine Fernandes <valentineinfantinejohn@gmail.com>
2022-04-24 01:29:42 -0800

    Create circle.cpp

---
+++ circle.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int rad;
+
+float PI = 3.14, area, ci;
+cout<<"Enter radius of circle: ";
+cin>>rad;
+area = PI * rad * rad;
+cout<<"Area of circle "<< area<<endl;
+ci = 2 * PI * rad;
+cout<<"Circumference of circle "<< ci<<endl;
+return (0);
+}
+
