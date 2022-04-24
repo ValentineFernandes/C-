35ae428152b5415a27bf921634a29eb2cc53fa0d
Valentine Fernandes <valentineinfantinejohn@gmail.com>
2022-04-24 01:46:28 -0800

    Create vowel.cpp

---
+++ vowel.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+  char a ;
+  
+ cout<<"Enter The Character You Want Check Vowel or Not\n\n";
+  cin>>a;
+
+  if(a=='a'||a=='e'||a=='i'||a=='o'||a=='u'||a=='A'||a=='E'||a=='I'||a=='O'||a=='U')
+  {
+    cout<<"Character is Vowel \n";
+  }
+  else
+  {
+   cout<<"Character is Not Vowel\n\n";
+ }
+ return 0;
+
+}
